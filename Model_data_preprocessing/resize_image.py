0be731cdaff5e105e1029cdcb0ff834378fda671
Suman <Sumanpandey72@gmail.com>
2021-04-10 14:13:05 -0500

    4

--- Model_data_preprocessing/resize_image.py
+++ Model_data_preprocessing/resize_image.py
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+#==========================================================================================
+#resize image
 from PIL import Image
 import os, sys
 
@@ -13,20 +14,50 @@ def resize():
             imResize = im.resize((28,28), Image.ANTIALIAS)
             imResize.save(f + 'r.jpg', 'JPEG', quality=90)
 
-=======
-from PIL import Image
-import os, sys
-
-path = "C:/Users/suman/Desktop/test_dataset/asl_alphabet_train/asl_alphabet_train/B/"
-dirs = os.listdir( path )
-
-def resize():
-    for item in dirs:
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
-            imResize = im.resize((28,28), Image.ANTIALIAS)
-            imResize.save(f + 'r.jpg', 'JPEG', quality=90)
-
->>>>>>> 5655fa7d12e3a971a4f5d4c17ff968dbedcc5a5a
 resize()
+
+#=================================================================================================
+# convert image to .csv file
+
+from PIL import Image
+import numpy as np
+import sys
+import os
+import csv
+
+# default format can be changed as needed
+def createFileList(myDir, format='.jpg'):
+    fileList = []
+    print(myDir)
+    for root, dirs, files in os.walk(myDir, topdown=False):
+        for name in files:
+            if name.endswith(format):
+                fullName = os.path.join(root, name)
+                fileList.append(fullName)
+    return fileList
+
+# load the original image
+myFileList = createFileList('C:/Users/suman/Desktop/test_dataset/asl_alphabet_train/asl_alphabet_train/B/')
+
+for file in myFileList:
+    print(file)
+    img_file = Image.open(file)
+    # img_file.show()
+
+    # get original image parameters...
+    width, height = img_file.size
+    format = img_file.format
+    mode = img_file.mode
+
+    # Make image Greyscale
+    img_grey = img_file.convert('L')
+    #img_grey.save('result.png')
+    #img_grey.show()
+
+    # Save Greyscale values
+    value = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
+    value = value.flatten()
+    print(value)
+    with open("test/test1.csv", 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(value)
--- Model_data_preprocessing/sort_csv.py
+++ Model_data_preprocessing/sort_csv.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pandas as pd
 import string
 
@@ -9,16 +8,4 @@ alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'
 for i in range (0,25):
     df_new = df.loc[df['label'] == i]
     filename = str(alpha[i])+".csv"
-=======
-import pandas as pd
-import string
-
-df = pd.read_csv("sign_mnist_test.csv")
-
-alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
-for i in range (0,25):
-    df_new = df.loc[df['label'] == i]
-    filename = str(alpha[i])+".csv"
->>>>>>> 5655fa7d12e3a971a4f5d4c17ff968dbedcc5a5a
     df_new.to_csv(filename, index = False)
