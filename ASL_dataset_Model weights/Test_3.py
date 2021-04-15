7365be37c28a799e2a8a80e92b7958f8d3f203cc
Bijesh Patel Vachanni <bijeshpatel369@gmail.com>
2021-04-14 21:55:25 -0500

    Asl Class update

--- ASL_dataset_Model weights/Test_3.py
+++ ASL_dataset_Model weights/Test_3.py
@@ -3,11 +3,10 @@ import numpy as np
 from tensorflow import keras
 from keras.models import load_model
 from PIL import Image
-#from keras.models import model_from_json
 
-model = keras.models.load_model('D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/ASL_dataset_Model weights/ASL_model.h5')
-pred_list = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O','P','Q','R','S','space','T','U','V','W','X','Y','Z']
-
+model = keras.models.load_model('D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/ASL_dataset_Model weights/ASL_Model_Weights.h5')
+#model = load_model('ASL_Model_Weights.h5')
+pred_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 for j in range(0,26):
     img = cv2.imread('D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/ASL_dataset_Model weights/Test_Images/'+pred_list[j]+'_test.jpg')
