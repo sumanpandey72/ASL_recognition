de2b17e34e6b4b76b987d4f483260e42866af750
Suman <Sumanpandey72@gmail.com>
2021-04-17 11:56:29 -0500

    test5

--- Model_deployment/main.py
+++ Model_deployment/main.py
@@ -16,6 +16,7 @@ window = tk.Tk()  #Makes main window
 #window.title("Reading Sign Language")
 window.wm_title("Reading Hand Signals")
 window.config(background="#FFFFFF")
+
 #canvas = Canvas(height=1000, width=1000, bg='#B1DDC6', highlightbackground="#B1DDC6")
 
 #Title in window
@@ -24,7 +25,7 @@ window.config(background="#FFFFFF")
 #Graphics window
 #imageFrame = canvas.create_image(100,100)
 imageFrame = tk.Frame(window, width=600, height=500)
-imageFrame.grid(row=1, column=0, padx=10, pady=2)
+imageFrame.grid(row=1, column=0)
 
 
 #Capture video frames
@@ -35,19 +36,22 @@ lmain.grid(row=1, column=0)
 video = cv2.VideoCapture(0)
 
 upper_left = (350, 50)
-bottom_right = (600, 350)
+bottom_right = (650, 350)
 
-model = keras.models.load_model('D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/ASL_dataset_Model weights/ASL_model.h5')
-pred_list = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O','P','Q','R','S','space','T','U','V','W','X','Y','Z']
+model = keras.models.load_model('D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/ASL_dataset_Model weights/ASL_Model_Weights.h5')
+pred_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
 def gray_live_video():
     static_back = None
+    i = 0
+    d = 0
     while True:
         check , frame = video.read()
         frame = cv2.flip(frame, 1)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        ROI = frame[50:350,350:600]
+        ROI = frame[50:350,350:650]
         ROI = cv2.flip(ROI,1)
+        ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2RGB)
         r = cv2.rectangle(frame, upper_left, bottom_right, (100, 50, 200), 1)
         gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -58,29 +62,33 @@ def gray_live_video():
 
         diff_ROI = cv2.absdiff(static_back, gray) 
 
-        # thresh_ROI = cv2.threshold(diff_ROI, 15, 255, cv2.THRESH_BINARY)[1] 
-        # thresh_ROI = cv2.dilate(thresh_ROI, None, iterations = 2) 
-
-        thresh_ROI= cv2.threshold(diff_ROI, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh_ROI= cv2.threshold(diff_ROI, 25, 255, cv2.THRESH_BINARY)[1]
         thresh_ROI= cv2.erode(thresh_ROI, None, iterations=2)
         thresh_ROI= cv2.dilate(thresh_ROI, None, iterations=2)
+        
 
-        cnts,_ = cv2.findContours(thresh_ROI.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        cv2.imshow("fa",thresh_ROI)
+        cnts,_ = cv2.findContours(thresh_ROI.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
         for contour in cnts: 
-            ROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2RGB)
-            cv2.imshow("output",ROI)
-            resized = cv2.resize(ROI, (28,28), interpolation = cv2.INTER_CUBIC)
-            cv2.imwrite("Output.jpg",ROI)
+            # if cv2.contourArea(contour) > 200: 
+            #     continue
+        
+            # print(cv2.contourArea(contour))
 
-            # predict the model
-            imgs = resized.reshape(-1,28,28,3)
+            cv2.imshow("output",ROI)
+
+            # filename = "D:/Grad/CSCE 5214 Soft dev for AI/ASL_recognition-1/hand/%s.jpg"%d
+            # cv2.imwrite(filename, ROI)
+            # d=pred_list[i]
+            # i+=1
+            imgs = cv2.cvtColor(ROI, cv2.COLOR_BGR2RGB)
+            imgs = cv2.resize(imgs, (28,28), interpolation = cv2.INTER_CUBIC)
+            imgs = imgs.reshape(-1,28,28,3)
             imgs = np.array(imgs)
             imgs = imgs.astype('float32')/255.0
-            pred = model.predict_classes(imgs)
-            print(pred_list[pred[0]])
 
+            pred = model.predict_classes(imgs)
+            #print(pred_list[pred[0]])
             tk.Label(window,text=pred_list[pred[0]],font=(None, 30)).grid(row=3,column=0)
           
         return frame
